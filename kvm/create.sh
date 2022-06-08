c542d275072234d5adde31f536cb8e02c7828c40
Michael Valdron <michael.valdron@gmail.com>
2022-06-08 00:21:47 -0400

    create vm script added.
    Signed-off-by: Michael Valdron <michael.valdron@gmail.com>

---
+++ kvm/create.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Check if hypervisor is installed
+if [ -z "$(which virsh)" ]
+then
+    echo "libvirt is not installed."
+    exit 1
+# Check if running as root or privileged user
+elif [ -z "$(groups | tr ' ' '\n' | grep libvirt)" ] && [ "$EUID" -ne 0 ]
+then
+    echo "Please run as root or user under 'libvirt' group."
+    exit 2
+fi
+
+# Variable
+base_dir=$(dirname $0)
+template_name=$1
+vm_name=$2
+
+# Create VM from template
+virsh define $base_dir/config/${template_name}.xml 1> /dev/null
+
+# Label VM
+virsh domrename ${template_name}-temp ${vm_name} 1> /dev/null
+
