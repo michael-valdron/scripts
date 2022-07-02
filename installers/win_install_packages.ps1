6f0d24254f489fbc1c32b4b48300b4f7f80d64a4
Michael Valdron <michael.valdron@gmail.com>
2022-07-02 00:04:02 -0400

    Gpg4win package added to Windows installer script.
    Signed-off-by: Michael Valdron <michael.valdron@gmail.com>

--- installers/win_install_packages.ps1
+++ installers/win_install_packages.ps1
@@ -24,7 +24,8 @@ winget install -h 7zip.7zip Waterfox.Waterfox Microsoft.VisualStudioCode Microso
     Microsoft.WindowsTerminal WinSCP.WinSCP GIMP.GIMP Adobe.Acrobat.Reader.64-bit TheDocumentFoundation.LibreOffice.LTS `
     KDE.Kdenlive OBSProject.OBSStudio calibre.calibre KeePassXCTeam.KeePassXC VideoLAN.VLC DelugeTeam.Deluge Cyanfish.NAPS2 `
     HandBrake.HandBrake Telegram.TelegramDesktop GuinpinSoft.MakeMKV SlackTechnologies.Slack Postman.Postman Microsoft.Skype `
-    AnyDeskSoftwareGmbH.AnyDesk Foundry376.Mailspring Zotero.Zotero Balena.Etcher Duplicati.Duplicati OpenVPNTechnologies.OpenVPN
+    AnyDeskSoftwareGmbH.AnyDesk Foundry376.Mailspring Zotero.Zotero Balena.Etcher Duplicati.Duplicati OpenVPNTechnologies.OpenVPN `
+    GnuPG.Gpg4win
 if ($LASTEXITCODE -ne 0) {
     Write-Error 'Failed to install winget packages.'
     exit $LASTEXITCODE
