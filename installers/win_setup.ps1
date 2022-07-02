6eb35119d3fb5a5b171c5cd78515cd84afe2c039
Michael Valdron <michael.valdron@gmail.com>
2022-07-01 21:36:36 -0400

    Setup script source added.
    Signed-off-by: Michael Valdron <michael.valdron@gmail.com>

--- installers/win_setup.ps1
+++ installers/win_setup.ps1
@@ -9,3 +9,38 @@ if (!$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Adminis
     exit 1
 }
 
+# Check if winget is installed
+try {
+    # Install git if winget is installed
+    winget install git
+}
+catch {
+    # If not, install winget then git
+    Write-Output 'winget is not installed, attempting install..'
+    $winget_version = (Invoke-WebRequest "https://api.github.com/repos/microsoft/winget-cli/releases/latest" | ConvertFrom-Json)[0].tag_name
+    $winget_package = "$base_dir/winget.msixbundle"
+    Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/$winget_version/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" `
+        -OutFile $winget_package
+    Add-AppxPackage -Path $winget_package -Confirm
+
+    try {
+        winget install git
+    }
+    catch {
+        Write-Error 'winget did not install correctly'
+        exit 2
+    }
+}
+
+# Using git, download and install the Windows 10 Debloater scripts to debloat Windows
+try {
+    $windebloater_root = "$base_dir/windows10debloater"
+    git clone "https://github.com/Sycnex/Windows10Debloater.git" $windebloater_root
+    Set-ExecutionPolicy Unrestricted -Force
+    "$windebloater_root\Windows10SysPrepDebloater.ps1 -SysPrep -Debloat -Privacy"
+    exit $LASTEXITCODE
+}
+catch {
+    Write-Error 'git did not install correctly'
+    exit 3
+}
