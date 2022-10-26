cdb86f9a9b909e66137770b091318fa9377c7a9f
Michael Valdron <michael.valdron@gmail.com>
2022-10-26 03:32:34 -0400

    yadm repo for EL installs added (#41)
    * manual repo importing for fedora refactored.
    * centos 8 stream yadm repo added for el8 and el9
    * yadm repo import added to server install script.
    * server install includes yadm.
    
    Signed-off-by: Michael Valdron <michael.valdron@gmail.com>

--- installers/server_install_packages.sh
+++ installers/server_install_packages.sh
@@ -8,13 +8,20 @@ then
 fi
 
 # Variables
+yum_repos_dir=/etc/yum.repos.d
 base_dir=$(dirname $0)
 
 # Run Rocky Linux setup script
 sh $base_dir/rocky_setup.sh
 
+# Add yadm repository, if not exist
+if [ ! -f $yum_repos_dir/home:TheLocehiliosan:yadm.repo ] && [ ! -f $yum_repos_dir/yadm.repo ];
+then
+    cp $base_dir/repos/$(rpm --eval "%{dist}" | sed 's/.//')/yadm.repo $yum_repos_dir/yadm.repo
+fi
+
 # Install packages
-dnf -y install htop neofetch tmux zsh podman podman-compose podman-docker samba git
+dnf -y install htop neofetch tmux zsh podman podman-compose podman-docker samba git yadm
 
 # Install ZFS
 sh $base_dir/packages/zfs/rocky_install.sh
