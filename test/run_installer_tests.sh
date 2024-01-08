f7dc4ece4bef1e26da84aec7e487b117fd458e69
Michael Valdron <14838510+michael-valdron@users.noreply.github.com>
2024-01-07 21:38:42 -0500

    replace github testing workflows with testing scripts

---
+++ test/run_installer_tests.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+BASE_DIR=$(dirname $0)
+
+echo "Test Fedora Installer.."
+sh ${BASE_DIR}/fedora/test.sh ${BASE_DIR}/../installers/fedora_install_packages.sh
+
+# Check if script did not exit properly
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+echo "Test Fedora Gaming Installer.."
+sh ${BASE_DIR}/fedora/test.sh ${BASE_DIR}/../installers/fedoragaming_install_packages.sh
+
+# Check if script did not exit properly
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+echo "Test EL 8 Server Installer.."
+sh ${BASE_DIR}/rocky/el8/test.sh ${BASE_DIR}/../installers/server_install_packages.sh
+
+# Check if script did not exit properly
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+echo "Test EL 9 Server Installer.."
+sh ${BASE_DIR}/rocky/el9/test.sh ${BASE_DIR}/../installers/server_install_packages.sh
+
+# Check if script did not exit properly
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+echo "Test Debian Buster Nameserver Installer.."
+sh ${BASE_DIR}/debian/buster/test.sh ${BASE_DIR}/../installers/dns_install_packages.sh
+
+# Check if script did not exit properly
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+echo "Test Debian Bullseye Nameserver Installer.."
+sh ${BASE_DIR}/debian/bullseye/test.sh ${BASE_DIR}/../installers/dns_install_packages.sh
+
+# Check if script did not exit properly
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+echo "Test EL 8 Cloud Installer.."
+sh ${BASE_DIR}/rocky/el8/test.sh ${BASE_DIR}/../installers/cloud_install_packages.sh
+
+# Check if script did not exit properly
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+echo "Test Manjaro Gaming Installer.."
+sh ${BASE_DIR}/archlinux/test.sh ${BASE_DIR}/../installers/manjaro_gaming_install_packages.sh
+
+# Check if script did not exit properly
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+echo "All installers completed succussfully!"
---
+++ test/run_setup_tests.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+BASE_DIR=$(dirname $0)
+
+echo "Test Fedora Setup Script.."
+sh ${BASE_DIR}/fedora/test.sh ${BASE_DIR}/../installers/fedora_setup.sh
+
+# Check if script did not exit properly
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+echo "Test EL 8 Setup Script.."
+sh ${BASE_DIR}/rocky/el8/test.sh ${BASE_DIR}/../installers/rocky_setup.sh
+
+# Check if script did not exit properly
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+echo "Test EL 9 Setup Script.."
+sh ${BASE_DIR}/rocky/el9/test.sh ${BASE_DIR}/../installers/rocky_setup.sh
+
+# Check if script did not exit properly
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+echo "Test Debian Buster Setup Script.."
+sh ${BASE_DIR}/debian/buster/test.sh ${BASE_DIR}/../installers/debian_setup.sh
+
+# Check if script did not exit properly
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+echo "Test Debian Bullseye Setup Script.."
+sh ${BASE_DIR}/debian/bullseye/test.sh ${BASE_DIR}/../installers/debian_setup.sh
+
+# Check if script did not exit properly
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+echo "All installers completed succussfully!"
