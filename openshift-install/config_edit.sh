730ad76c939cddc8b0132ac43296dbb7401924ad
Michael Valdron <14838510+michael-valdron@users.noreply.github.com>
2024-06-20 15:23:38 -0400

    openshift-install/config_edit.sh: fix openshift-install working installation directory

--- openshift-install/config_edit.sh
+++ openshift-install/config_edit.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
+OPENSHIFT_INSTALL_DIR=${OPENSHIFT_INSTALL_DIR:-'~/.openshift-install'}
+
 if [ $# -ne 1 ]
 then
     echo "error: expecting one argument: sh config_edit.sh <cluster_name>"
     exit 1
 fi
 
-if [ -f "${1}.yaml" ]
+if [ -f "${OPENSHIFT_INSTALL_DIR}/${1}.yaml" ]
 then
-    ${EDITOR} ~/.openshift-install/${1}.yaml
-elif [ -f "${1}.yml" ]
+    ${EDITOR} ${OPENSHIFT_INSTALL_DIR}/${1}.yaml
+elif [ -f "${OPENSHIFT_INSTALL_DIR}/${1}.yml" ]
 then
-    ${EDITOR} ~/.openshift-install/${1}.yml
+    ${EDITOR} ${OPENSHIFT_INSTALL_DIR}/${1}.yml
 else
     echo "error: unknown cluster profile '${1}'"
     exit 1
