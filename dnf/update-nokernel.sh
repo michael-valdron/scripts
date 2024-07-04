ebae69673d9a2a156128cab804bb298bc69cb88b
Michael Valdron <14838510+michael-valdron@users.noreply.github.com>
2024-07-03 23:56:45 -0400

    no kernel update script now defines kernel packages under a constant

--- dnf/update-nokernel.sh
+++ dnf/update-nokernel.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+KERNEL_PACKAGES='kernel kernel-core kernel-devel kernel-devel-matched 
+kernel-modules kernel-modules-core kernel-modules-extra'
 BASE_DIR=$(dirname $0)
 
-sh ${BASE_DIR}/update.sh --exclude="kernel kernel-core kernel-devel kernel-devel-matched kernel-modules kernel-modules-core kernel-modules-extra" $@
+sh ${BASE_DIR}/update.sh --exclude=${KERNEL_PACKAGES} $@
+
