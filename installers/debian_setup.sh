7bc0435d99ebad6ccc94112255e42ed55ccdd60a
Michael Valdron <14838510+michael-valdron@users.noreply.github.com>
2024-07-06 22:09:56 -0400

    fix debian setup

--- installers/debian_setup.sh
+++ installers/debian_setup.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-# Check if running as root
-if [ "$EUID" -ne 0 ]
-then 
-    echo "Please run as root"
-    exit 1
-fi
-
 # Update package lists and upgrade installed packages
-apt-get update -q && apt-get upgrade -yq
+if [ ! -z "$(command -v sudo)" ]
+then
+    sudo apt-get update -q && apt-get upgrade -yq
+else
+    if [ $(whoami) != 'root' ]
+    then
+        echo "error: neither is root nor is sudo installed."
+        exit 1
+    else
+        apt-get update -q && apt-get upgrade -yq
+    fi
+fi
