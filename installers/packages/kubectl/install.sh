664dc4d494f095bf68628bc80e7f9da94dfccdae
Michael Valdron <14838510+michael-valdron@users.noreply.github.com>
2026-02-01 03:09:44 -0500

    fix if statements under kubectl install script

--- installers/packages/kubectl/install.sh
+++ installers/packages/kubectl/install.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 
-if [ -z ${KUBECTL_DL_PATH} ]
-then
+if [ -z ${KUBECTL_DL_PATH} ]; then
     KUBECTL_DL_PATH=/tmp/kubectl
 fi
 
-if [ -z ${KUBECTL_INSTALL_PATH} ]
-then
+if [ -z ${KUBECTL_INSTALL_PATH} ]; then
     KUBECTL_INSTALL_PATH=/usr/local/bin/kubectl
 fi
 
-if [ "$(arch)" == "x86_64" ]
-then
+if [ "$(arch)" = "x86_64" ]; then
     arch_platform="amd64"
-elif [ "$(arch)" == "arm" ]
-then
+elif [ "$(arch)" = "arm" ]; then
     arch_platform="arm64"
 else
     echo "error: unsupported architecture '$(arch)'"
@@ -22,23 +18,20 @@ else
 fi
 
 curl -L https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/${arch_platform}/kubectl -o ${KUBECTL_DL_PATH}
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
     echo "error: problem downloading kubectl"
     exit 1
 fi
 
 curl -L "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/${arch_platform}/kubectl.sha256" -o ${KUBECTL_DL_PATH}.sha256
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
     echo "error: problem downloading kubectl.sha256"
     exit 1
 fi
 
 check=$(echo "$(cat ${KUBECTL_DL_PATH}.sha256) ${KUBECTL_DL_PATH}" | sha256sum --check)
 echo $check
-if [ "${check}" == "${KUBECTL_DL_PATH}: OK" ]
-then
+if [ "${check}" = "${KUBECTL_DL_PATH}: OK" ]; then
     sudo install -o root -g root -m 0755 ${KUBECTL_DL_PATH} ${KUBECTL_INSTALL_PATH}
 else
     echo "error: kubectl is not valid"
