3d347b5049092598a9467e569133b30b09987698
Michael Valdron <14838510+michael-valdron@users.noreply.github.com>
2026-05-12 21:36:15 -0400

    update test/run_setup_tests.sh use compose and remove EOL scripts

--- test/run_setup_tests.sh
+++ test/run_setup_tests.sh
@@ -1,41 +1,11 @@
 #!/bin/sh
 
+CONTAINER_ENGINE=${CONTAINER_ENGINE:-'docker'}
+
 BASE_DIR=$(dirname $0)
 
 echo "Test Fedora Setup Script.."
-sh ${BASE_DIR}/fedora/test.sh ${BASE_DIR}/../installers/fedora_setup.sh
-
-# Check if script did not exit properly
-if [ $? -ne 0 ]; then
-    exit $?
-fi
-
-echo "Test EL 8 Setup Script.."
-sh ${BASE_DIR}/rocky/el8/test.sh ${BASE_DIR}/../installers/rocky_setup.sh
-
-# Check if script did not exit properly
-if [ $? -ne 0 ]; then
-    exit $?
-fi
-
-echo "Test EL 9 Setup Script.."
-sh ${BASE_DIR}/rocky/el9/test.sh ${BASE_DIR}/../installers/rocky_setup.sh
-
-# Check if script did not exit properly
-if [ $? -ne 0 ]; then
-    exit $?
-fi
-
-echo "Test Debian Buster Setup Script.."
-sh ${BASE_DIR}/debian/buster/test.sh ${BASE_DIR}/../installers/debian_setup.sh
-
-# Check if script did not exit properly
-if [ $? -ne 0 ]; then
-    exit $?
-fi
-
-echo "Test Debian Bullseye Setup Script.."
-sh ${BASE_DIR}/debian/bullseye/test.sh ${BASE_DIR}/../installers/debian_setup.sh
+${CONTAINER_ENGINE} compose -f "${BASE_DIR}/compose.yml" run --rm test-fedora "${BASE_DIR}/../installers/fedora_setup.sh"
 
 # Check if script did not exit properly
 if [ $? -ne 0 ]; then
@@ -43,7 +13,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Test Debian Bookworm Setup Script.."
-sh ${BASE_DIR}/debian/bookworm/test.sh ${BASE_DIR}/../installers/debian_setup.sh
+${CONTAINER_ENGINE} compose -f "${BASE_DIR}/compose.yml" run --rm test-debian-bookworm "${BASE_DIR}/../installers/debian_setup.sh"
 
 # Check if script did not exit properly
 if [ $? -ne 0 ]; then
@@ -51,7 +21,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Test Debian Bookworm Docker Setup Script.."
-sh ${BASE_DIR}/debian/bookworm/test.sh ${BASE_DIR}/../installers/debian_docker_setup.sh
+${CONTAINER_ENGINE} compose -f "${BASE_DIR}/compose.yml" run --rm test-debian-bookworm "${BASE_DIR}/../installers/debian_docker_setup.sh"
 
 # Check if script did not exit properly
 if [ $? -ne 0 ]; then
