b076c086ab540c0ebd434673d9d53612a8e56f67
Michael Valdron <14838510+michael-valdron@users.noreply.github.com>
2026-05-12 21:36:33 -0400

    update test/run_installer_tests.sh use compose and remove EOL scripts

--- test/run_installer_tests.sh
+++ test/run_installer_tests.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+CONTAINER_ENGINE=${CONTAINER_ENGINE:-'docker'}
+
 BASE_DIR=$(dirname $0)
 
 echo "Test Fedora Installer.."
-sh ${BASE_DIR}/fedora/test.sh ${BASE_DIR}/../installers/fedora_install_packages.sh
+${CONTAINER_ENGINE} compose -f "${BASE_DIR}/compose.yml" run --rm test-fedora "${BASE_DIR}/../installers/fedora_install_packages.sh"
 
 # Check if script did not exit properly
 if [ $? -ne 0 ]; then
@@ -11,39 +13,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Test Fedora Gaming Installer.."
-sh ${BASE_DIR}/fedora/test.sh ${BASE_DIR}/../installers/fedoragaming_install_packages.sh
-
-# Check if script did not exit properly
-if [ $? -ne 0 ]; then
-    exit $?
-fi
-
-echo "Test EL 8 Server Installer.."
-sh ${BASE_DIR}/rocky/el8/test.sh ${BASE_DIR}/../installers/server_install_packages.sh
-
-# Check if script did not exit properly
-if [ $? -ne 0 ]; then
-    exit $?
-fi
-
-echo "Test EL 9 Server Installer.."
-sh ${BASE_DIR}/rocky/el9/test.sh ${BASE_DIR}/../installers/server_install_packages.sh
-
-# Check if script did not exit properly
-if [ $? -ne 0 ]; then
-    exit $?
-fi
-
-echo "Test Debian Buster Nameserver Installer.."
-sh ${BASE_DIR}/debian/buster/test.sh ${BASE_DIR}/../installers/dns_install_packages.sh
-
-# Check if script did not exit properly
-if [ $? -ne 0 ]; then
-    exit $?
-fi
-
-echo "Test Debian Bullseye Nameserver Installer.."
-sh ${BASE_DIR}/debian/bullseye/test.sh ${BASE_DIR}/../installers/dns_install_packages.sh
+${CONTAINER_ENGINE} compose -f "${BASE_DIR}/compose.yml" run --rm test-fedora "${BASE_DIR}/../installers/fedoragaming_install_packages.sh"
 
 # Check if script did not exit properly
 if [ $? -ne 0 ]; then
@@ -51,7 +21,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Test Debian Bookworm Nameserver Installer.."
-sh ${BASE_DIR}/debian/bookworm/test.sh ${BASE_DIR}/../installers/dns_install_packages.sh
+${CONTAINER_ENGINE} compose -f "${BASE_DIR}/compose.yml" run --rm test-debian-bookworm "${BASE_DIR}/../installers/dns_install_packages.sh"
 
 # Check if script did not exit properly
 if [ $? -ne 0 ]; then
@@ -59,15 +29,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Test Debian Bookworm Server Installer.."
-sh ${BASE_DIR}/debian/bookworm/test.sh ${BASE_DIR}/../installers/debian_server_install_packages.sh
-
-# Check if script did not exit properly
-if [ $? -ne 0 ]; then
-    exit $?
-fi
-
-echo "Test EL 8 Cloud Installer.."
-sh ${BASE_DIR}/rocky/el8/test.sh ${BASE_DIR}/../installers/cloud_install_packages.sh
+${CONTAINER_ENGINE} compose -f "${BASE_DIR}/compose.yml" run --rm test-debian-bookworm "${BASE_DIR}/../installers/debian_server_install_packages.sh"
 
 # Check if script did not exit properly
 if [ $? -ne 0 ]; then
@@ -75,7 +37,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Test Manjaro Gaming Installer.."
-sh ${BASE_DIR}/archlinux/test.sh ${BASE_DIR}/../installers/manjaro_gaming_install_packages.sh
+${CONTAINER_ENGINE} compose -f "${BASE_DIR}/compose.yml" run --rm test-archlinux ${BASE_DIR}/../installers/manjaro_gaming_install_packages.sh
 
 # Check if script did not exit properly
 if [ $? -ne 0 ]; then
