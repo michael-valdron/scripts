aa11577c69d115c069f6f5578a0266e83d413f6c
Michael Valdron <14838510+michael-valdron@users.noreply.github.com>
2026-05-12 23:05:02 -0400

    util/apt/update-nokernel.sh: fix source for /bin/sh

--- util/apt/update-nokernel.sh
+++ util/apt/update-nokernel.sh
@@ -1,23 +1,12 @@
 #!/bin/sh
 
-KERNEL_PACKAGES=(
-    "linux-base"
-    "linux-firmware"
-    "linux-generic-hwe-*" 
-    "linux-headers-*" 
-    "linux-hwe-*" 
-    "linux-image-*"
-    "linux-libc-dev"
-    "linux-modules-*"
-    "linux-sound-base"
-    "linux-tools-*"
-)
+KERNEL_PACKAGES='linux-base linux-firmware linux-generic-hwe-* linux-headers-* linux-hwe-* linux-image-* linux-libc-dev linux-modules-* linux-sound-base linux-tools-*'
 
 if [ "$(command -v apt)" ]; then
 	sudo apt update  # Refresh package lists first!
-    for p in "${KERNEL_PACKAGES[@]}"; do sudo apt-mark hold $p; done
+    for p in "${KERNEL_PACKAGES}"; do sudo apt-mark hold $p; done
     sudo apt upgrade $@
-    for p in "${KERNEL_PACKAGES[@]}"; do sudo apt-mark unhold $p; done
+    for p in "${KERNEL_PACKAGES}"; do sudo apt-mark unhold $p; done
 else
 	echo "system does not use apt"
 	exit 1
